a8a8f37e116f96418857b9a7cd35a40b30bc5b6e
krazy-kudu <66883099+krazy-kudu@users.noreply.github.com>
2020-08-24 14:56:42 -0700

    query on my wayward soooon

--- queries.sql
+++ queries.sql
@@ -7,3 +7,11 @@ salaries.salary
 FROM employees
 INNER JOIN salaries ON
 employees.emp_no = salaries.emp_no;
+
+-- 2) List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.emp_no,
+employees.first_name,
+employees.last_name,
+employees.hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM employees.hire_date) = 1986;
