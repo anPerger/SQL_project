eb24a59f2051d02356941db034cbfc6850dd0a5e
krazy-kudu <66883099+krazy-kudu@users.noreply.github.com>
2020-08-24 14:43:11 -0700

    something new

--- schema.sql
+++ schema.sql
@@ -15,9 +15,10 @@ CREATE TABLE IF NOT EXISTS employees
 
 CREATE TABLE IF NOT EXISTS dept_manager
 (
-    emp_no INTEGER NOT NULL,
+    primary_key INTEGER NOT NULL,
     dept_no VARCHAR(30),
-    PRIMARY KEY(emp_no)
+    emp_no INTEGER,
+    PRIMARY KEY(primary_key)
 );
 
 CREATE TABLE IF NOT EXISTS dept_emp
@@ -47,23 +48,65 @@ CREATE TABLE IF NOT EXISTS titles
     PRIMARY KEY(title_id)
 );
 
+CREATE TABLE IF NOT EXISTS dept_emp
+(
+    primary_key INTEGER NOT NULL,
+    emp_no INTEGER,
+    dept_no VARCHAR(30),
+    PRIMARY KEY(primary_key)
+);
+
+CREATE TABLE IF NOT EXISTS titles
+(
+    primary_key INTEGER NOT NULL,
+    title_id VARCHAR(30),
+    title VARCHAR(30),
+    PRIMARY KEY(primary_key)
+);
+
+CREATE TABLE IF NOT EXISTS salaries
+(
+    primary_key INTEGER NOT NULL,
+    emp_no INTEGER,
+    salary INTEGER,
+    PRIMARY KEY(primary_key)
+);
+
 
 -- Create FKs
+ALTER TABLE departments
+    ADD    FOREIGN KEY (dept_no)
+    REFERENCES dept_manager(dept_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_emp
+    ADD    FOREIGN KEY (dept_no)
+    REFERENCES departments(dept_name)
+    MATCH SIMPLE
+;
+    
 ALTER TABLE employees
     ADD    FOREIGN KEY (emp_no)
-    REFERENCES dept_manager(emp_no)
+    REFERENCES dept_emp(emp_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE titles
+    ADD    FOREIGN KEY (title_id)
+    REFERENCES employees(emp_title_id)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE salaries
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES employees(emp_no)
     MATCH SIMPLE
 ;
     
 ALTER TABLE dept_manager
     ADD    FOREIGN KEY (emp_no)
     REFERENCES employees(emp_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE departments
-    ADD    FOREIGN KEY (dept_no)
-    REFERENCES dept_manager(dept_no)
     MATCH SIMPLE
 ;
     
