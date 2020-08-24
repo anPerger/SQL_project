5d054c0bc3c3c3cf2876735f6c07b9336c64c216
krazy-kudu <66883099+krazy-kudu@users.noreply.github.com>
2020-08-24 14:58:59 -0700

    lay your weary head to rest

--- queries.sql
+++ queries.sql
@@ -27,3 +27,14 @@ INNER JOIN dept_manager
 ON employees.emp_no = dept_manager.emp_no
 JOIN departments
 ON departments.dept_no = dept_manager.dept_no;
+
+-- 4) List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+employees.first_name,
+employees.last_name,
+departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+JOIN departments
+ON departments.dept_no = dept_emp.dept_no;
