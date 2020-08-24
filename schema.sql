02d97051c698e1d03b93d408de1ac2e933d626d6
krazy-kudu <66883099+krazy-kudu@users.noreply.github.com>
2020-08-24 14:54:17 -0700

    starting queries

--- schema.sql
+++ schema.sql
@@ -1,39 +1,32 @@
 -- Create schemas
 
 -- Create tables
-CREATE TABLE IF NOT EXISTS employees
-(
-    emp_no INTEGER NOT NULL,
-    emp_title_id VARCHAR(30),
-    birth_date DATE,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    sex VARCHAR(1),
-    hire_date DATE,
-    PRIMARY KEY(emp_no)
-);
-
-CREATE TABLE IF NOT EXISTS dept_manager
-(
-    primary_key INTEGER NOT NULL,
-    dept_no VARCHAR(30),
-    emp_no INTEGER,
-    PRIMARY KEY(primary_key)
-);
-
-CREATE TABLE IF NOT EXISTS dept_emp
-(
-    primary_key INTEGER NOT NULL,
-    PRIMARY KEY(primary_key)
-);
-
 CREATE TABLE IF NOT EXISTS departments
 (
-    dept_no VARCHAR(30) NOT NULL,
+    dept_no VARCHAR(30) NOT NULL UNIQUE,
     dept_name VARCHAR(30),
     PRIMARY KEY(dept_no)
 );
 
+CREATE TABLE IF NOT EXISTS employees
+(
+    emp_no INTEGER NOT NULL UNIQUE,
+    emp_title_id VARCHAR(30) NOT NULL,
+    birth_date DATE,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    sex VARCHAR(30),
+    hire_date DATE,
+    PRIMARY KEY(emp_no)
+);
+
+CREATE TABLE IF NOT EXISTS titles
+(
+    title_id VARCHAR(30) NOT NULL UNIQUE,
+    title VARCHAR(30),
+    PRIMARY KEY(title_id)
+);
+
 CREATE TABLE IF NOT EXISTS salaries
 (
     emp_no INTEGER NOT NULL,
@@ -41,67 +34,37 @@ CREATE TABLE IF NOT EXISTS salaries
     PRIMARY KEY(emp_no)
 );
 
-CREATE TABLE IF NOT EXISTS titles
-(
-    title_id VARCHAR(30) NOT NULL,
-    title VARCHAR(30),
-    PRIMARY KEY(title_id)
-);
-
 CREATE TABLE IF NOT EXISTS dept_emp
 (
-    primary_key INTEGER NOT NULL,
-    emp_no INTEGER,
-    dept_no VARCHAR(30),
-    PRIMARY KEY(primary_key)
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+    PRIMARY KEY(emp_no, dept_no)
 );
 
-CREATE TABLE IF NOT EXISTS titles
+CREATE TABLE IF NOT EXISTS dept_manager
 (
-    primary_key INTEGER NOT NULL,
-    title_id VARCHAR(30),
-    title VARCHAR(30),
-    PRIMARY KEY(primary_key)
-);
-
-CREATE TABLE IF NOT EXISTS salaries
-(
-    primary_key INTEGER NOT NULL,
-    emp_no INTEGER,
-    salary INTEGER,
-    PRIMARY KEY(primary_key)
-);
-
-CREATE TABLE IF NOT EXISTS emp_title
-(
-    title_id VARCHAR(30) NOT NULL,
-    emp_id INTEGER NOT NULL,
-    PRIMARY KEY(title_id, emp_id)
+    dept_no VARCHAR(30) NOT NULL,
+    emp_no INTEGER NOT NULL UNIQUE,
+    PRIMARY KEY(emp_no)
 );
 
 
 -- Create FKs
-ALTER TABLE departments
+ALTER TABLE dept_manager
     ADD    FOREIGN KEY (dept_no)
-    REFERENCES dept_manager(dept_no)
+    REFERENCES departments(dept_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_emp
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES employees(emp_no)
     MATCH SIMPLE
 ;
     
 ALTER TABLE dept_emp
     ADD    FOREIGN KEY (dept_no)
-    REFERENCES departments(dept_name)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE employees
-    ADD    FOREIGN KEY (emp_no)
-    REFERENCES dept_emp(emp_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE salaries
-    ADD    FOREIGN KEY (emp_no)
-    REFERENCES employees(emp_no)
+    REFERENCES departments(dept_no)
     MATCH SIMPLE
 ;
     
@@ -111,15 +74,15 @@ ALTER TABLE dept_manager
     MATCH SIMPLE
 ;
     
-ALTER TABLE emp_title
-    ADD    FOREIGN KEY (emp_id)
-    REFERENCES employees(emp_no)
+ALTER TABLE employees
+    ADD    FOREIGN KEY (emp_title_id)
+    REFERENCES titles(title_id)
     MATCH SIMPLE
 ;
     
-ALTER TABLE emp_title
-    ADD    FOREIGN KEY (title_id)
-    REFERENCES titles(title_id)
+ALTER TABLE employees
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES salaries(emp_no)
     MATCH SIMPLE
 ;
     
